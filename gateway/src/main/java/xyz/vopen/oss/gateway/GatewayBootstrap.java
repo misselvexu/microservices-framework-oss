bff23b243f68db9f1d350cb4bb787e8e9541e015
misselvexu <iskp.me@gmail.com>
2019-01-04 18:04:42 +0800

    ADD GATEWAY CODE

---
+++ gateway/src/main/java/xyz/vopen/oss/gateway/GatewayBootstrap.java
@@ -0,0 +1,18 @@
+package xyz.vopen.oss.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * GatewayBootstrap
+ *
+ * @author <a href="mailto:iskp.me@gmail.com">Elve.Xu</a>
+ * @version ${project.version} - 2019-01-04.
+ */
+@SpringBootApplication
+public class GatewayBootstrap {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayBootstrap.class, args);
+  }
+}
