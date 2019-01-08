fc52db65ff0bd02b5d8fff97b946014a82713acb
misselvexu <iskp.me@gmail.com>
2019-01-08 16:46:52 +0800

    ADD DUBBO SUPPORT

--- services/auth/src/main/java/xyz/vopen/oss/services/auth/AuthProviderBootstrap.java
+++ services/auth/src/main/java/xyz/vopen/oss/services/auth/AuthProviderBootstrap.java
@@ -1,13 +1,14 @@
 package xyz.vopen.oss.services.auth;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 /**
  * AuthProviderBootstrap
  *
  * @author <a href="mailto:iskp.me@gmail.com">Elve.Xu</a>
  * @version ${project.version} - 2019-01-08.
  */
+@EnableAutoConfiguration
 public class AuthProviderBootstrap {
-
-
 
 }
