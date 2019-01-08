d36dd9a5a82dc41b91b24ea28c2d530118d0ca9f
misselvexu <iskp.me@gmail.com>
2019-01-08 16:34:57 +0800

    FIX DEPENDENCY ISSUES

---
+++ services/auth/src/main/java/xyz/vopen/oss/services/auth/AuthProviderBootstrap.java
@@ -0,0 +1,13 @@
+package xyz.vopen.oss.services.auth;
+
+/**
+ * AuthProviderBootstrap
+ *
+ * @author <a href="mailto:iskp.me@gmail.com">Elve.Xu</a>
+ * @version ${project.version} - 2019-01-08.
+ */
+public class AuthProviderBootstrap {
+
+
+
+}
